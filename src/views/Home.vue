2b21a4bcd0bdb819122ae83b79a15a625e6b7b89
WouterVan <wouter04316@gmail.com>
2018-10-21 16:06:28 +0200

    basic ui setup

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <appointment/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Appointment from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Appointment
+  }
+}
+
+
+import vueSlider from 'vue-slider-component';
+
+export default {
+  components: {
+    vueSlider
+  },
+  data () {
+    return {
+      value: 0
+    }
+  }
+}
+
+
+</script>
---
+++ src/views/Users.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Users">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AdminDashboard from '@/components/User.vue'
+
+export default {
+  name: 'User',
+  components: {
+    AdminDashboard
+  }
+}
+</script>
